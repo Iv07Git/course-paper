082424d90b522d55ab229df03205b2bcee87549a
Home <ivan.latyshev.2015@yandex.ru>
2025-11-23 23:19:55 +0800

    Add tasks

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,11 @@
+import java.util.Objects;
+
+// 1. Создание класса Employee с информацией о Ф.И.О., отделе и зарплате.
 public class Employee {
     private String fullName;
     private int department;
     private int salary;
+    // 2. Добавление статистической переменной-счетчика, которая ривечает за id.
     private static int idCounter;
     private int id;
 
@@ -11,9 +15,12 @@ public class Employee {
         this.salary = salary;
     }
 
+    // 3. Добавление в класс Employee поля id.
     public Employee () {
         this.id = idCounter++;
     }
+
+    // 4. Скрытие прямого доступа к полям класса Employee, добавление возможности получать значения полей (геттеры) и устанавливать значение полей отдела и зарплаты (сеттеры).
 
     public String getFullName () {
         return this.fullName;
@@ -37,7 +44,7 @@ public class Employee {
 
     public void setDepartment(int department) {
         if (department < 1 || department > 5) {
-            throw new IllegalArgumentException("Существуют отделы от 1 до 5");
+            throw new IllegalArgumentException("Существуют отделы с 1 по 5");
         }
         this.department = department;
     }
@@ -49,10 +56,19 @@ public class Employee {
         this.salary = salary;
     }
 
+    // 5. Реализация в классе Employee контракта equals.
+
     @Override
     public boolean equals(Object other) {
-        if () {
-
+        if (this.getClass() != other.getClass()) {
+            return false;
         }
+        Employee salaries = (Employee) other;
+        return salaries.equals(salaries.salary);
+    }
+
+    @Override
+    public int hashCode () {
+        return java.util.Objects.hash(salary);
     }
 }
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,2 +1,8 @@
+// 7. Создание класса EmployeeBook с полем типа Employee[10], выполняющеероль хранилища для записей о сотрудниках.
 public class EmployeeBook {
+    private Employee [] employees = new Employee[10];
+
+    public EmployeeBook () {
+        this.employees = employees;
+    }
 }
