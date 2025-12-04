112e2afe1c96679a11463c2f3c920dc6d7d7df95
ivanl <ivan.latyshev.2015@yandex.ru>
2025-12-04 21:59:07 +0800

    Add task

--- src/Employee.java
+++ src/Employee.java
@@ -2,24 +2,36 @@ import java.util.Objects;
 
 // 1. Создание класса Employee с информацией о Ф.И.О., отделе и зарплате.
 public class Employee {
-    private String fullName;
+    private final String surname;
+    private final String name;
+    private final String patronymic;
     private int department;
     private int salary;
     // 2. Добавление статистической переменной-счетчика, которая отвечает за id.
-    private static int idCounter;
+    private static int idCounter = 1;
     // 3. Добавление в класс Employee поле id.
-    private int id;
+    private final int id;
 
-    public Employee (String fullName, int department, int salary) {
-        this.fullName = fullName;
+    public Employee (String surname, String name, String patronymic, int department, int salary) {
+        id = idCounter++;
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
         this.department = department;
         this.salary = salary;
-        id = idCounter++;
     }
 
     // 4. Скрытие прямого доступа к полям класса Employee, добавление возможности получать значения полей (геттеры) и устанавливать значение полей отдела и зарплаты (сеттеры).
-    public String getFullName () {
-        return fullName;
+    public String getSurname () {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
     }
 
     public int getDepartment() {
@@ -51,25 +63,30 @@ public class Employee {
     // 5. Реализация контракта equals
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(fullName, employee.fullName);
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Employee salary = (Employee) o;
+        return this.salary.equals(salaries.salary) && this.fullName.equals(salaries.fullName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fullName, department, salary, id);
+        return Objects.hash(surname, name, patronymic, department, salary, id);
     }
 
     // 6. Реализация метода вывода информации о сотруднике.
     @Override
     public String toString () {
-        return "Сотрудник № " + idCounter + ": Ф.И.О. " + fullName + " работает в отделе №" +
+        return "Сотрудник № " + idCounter + " Ф.И.О.: " + surname + name + patronymic + " работает в отделе №" +
                 department + " и полуает зарплату " + salary + " рублей";
     }
 
     // Отдельный метод для вывода имени и зарплаты
     public void printShortInfo () {
-        System.out.println("Ф.И.О.: " + fullName + " зарплата - " + salary);
+        System.out.println("Ф.И.О.: " + surname + name + patronymic + " зарплата - " + salary);
     }
 }
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,26 +1,134 @@
 // 7. Создание класса EmployeeBook с полем типа Employee[10], выполняющее роль хранилища для записей о сотрудниках.
 public class EmployeeBook {
-    private static Employee[] employees = new Employee[10];
 
-    // 8. Создать методы, которые взаимодействуют с массивом пункта 7 и возвращают результат: - Получить список всех сотрудников со всеми данными о них, вывести в консоль значения всех полей, кроме Null
+    private final static Random RANDOM = new Random();
+    private final static String [] SURNAME ("Андреев", "Иванов", "Олегов", "Кириллов", "Александров", "Вадимов", "Алексеев");
+    private final static String [] NAME ("Андрей", "Иван", "Олег", "Кирилл", "Александр", "Вадим", "Алексей");
+    private final static String [] PATRONYMIC ("Андреевич", "Иванович", "Олегович", "Кириллович", "Александрович", "Вадимович", "Алексеевич");
+
+    private static Employee[] EMPLOYEES = new Employee[10];
+
+    // 8. Создать методы, которые взаимодействуют с массивом пункта 7 и возвращают результат:
+    // - Получить список всех сотрудников со всеми данными о них, вывести в консоль значения всех полей, кроме Null
     public static boolean listAllEmployee(Employee employee) {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null) {
-                employees[i] = employee;
-                return true;
-            }
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            String fullName = SURNAME [RANDOM.nextInt(0, SURNAME().length)] + " " +
+                NAME [RANDOM.nextInt(0, NAME().length)] + " " +
+                PATRONYMIC [RANDOM.nextInt(0, PATRONYMIC.length)];
+            EMPLOYEES[i] = new Employee (fullName, RANDOM.nextInt(1, 6), RANDOM.nextInt(50000, 450000))
         }
-        return false;
     }
 
     public void printAllEmployee() {
+        for (Employee employee : EMPLOYEE) {
+            if (employee == null) {
+                break;
+            }
+            System.out.println(employee.toString());
+        }
+    }
+}
+
+// - Подсчитать среднее значение зарплат.
+    public double averageSalary() {
+        double sum = 0;
+        int count = 0;
         for (Employee employee : employees) {
             if (employee == null) {
-                System.out.println(employee.toString());
+                break;
+            }
+            int salary = employee.getSalary();
+            if (salary != null) {
+                sum += salary;
+            }
+            count++;
+        }
+        if (count == 0) {
+            return 0;
+        }
+        return sum / count;
+    }
+
+    public void printTaxes(String taxType) {
+        for (Employee employee : employees) {
+            if (employee == null)
+                break;
+            int salary = employee.getSalary();
+            if (salary == null)
+                continue;
+
+            double taxPercent;
+            switch (taxType.toUpperCase()) {
+                case "PROPORYIONAL":
+                    taxPercent = 13;
+                    break;
+                case "PROGRESSIVE":
+                    if (salary <= 150) {
+                        taxPercent = 13;
+                    } else if (salary <= 350) {
+                        taxPercent = 17;
+                    } else {
+                        taxPercent = 21;
+                    }
+                    break;
+                default:
+                    System.out.println("Неверный тип расчета налогов: " + taxType);
+                    return;
+            }
+
+            double taxAmount = salary * taxPercent / 100;
+            System.out.printf("Employee: %s, Salary: %.2f, Tax (%s): %.2f%n",
+                    employee.toString(), salary, taxType.toUpperCase(), taxAmount);
+        }
+    }
+
+    public void indexSalaryByDepartment(int departmentNumber, double percent) {
+        for (Employee employee : employees) {
+            if (employee == null)
+                break;
+            if (!employee.getDepartment(.equals(departmentNumber)))
+            continue;
+            double oldSalary = employee.getSalary();
+            if (oldSalary == null)
+                continue;
+            double newSalary = oldSalary + oldSalary * percent / 100;
+            employee.setSalary(newSalary);
+        }
+    }
+    public void findFirstEmployeeByDepartmentAndSalary(int departmentNumber, double salaryThreshold) {
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (employee == null)
+                break;
+            if (employee.getDepartment().equals(departmentNumber) && employee.getSalary() > salaryThreshold) {
+                System.out.printf("First employee in department %d with salary > %.2f at position %d:%n", departmentNumber, salaryThreshold, i + 1);
+                employee.printShortInfo();
+                break;
             }
         }
     }
 
-    // Подсчитать среднее значение зарплат.
+    public void printEmployeesWithSalaryLessThan(double wage, int employeeNumber) {
+        int count = 0;
+        int index = 0;
+        while (index < employees.length && count < employeeNumber) {
+            Employee employee = employees[index];
+            if (employee == null) break;
+            if (employee.getSalary() < wage) {
+                employee.printShortInfo();
+                count++;
+            }
+            index++;
+        }
+    }
 
+    public Employee getEmployeeById(int id) {
+        for (Employee employee : employees) {
+            if (employee == null) break;
+            if (employee.getId() == id) {
+                return employee;
+            }
+        }
+        return null;
+    }
 }
